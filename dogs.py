231aa27c5959e37451f7726211c3d745889b963f
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-14 20:36:56 -0700

    Refactored My_Data_Splitter to OOP

---
+++ dogs.py
@@ -0,0 +1,25 @@
+from pdb import set_trace as breakpoint
+
+
+class Dog():
+    def __init__(self, name, age, housebroke):
+        self.name = name
+        self.age = age
+        self.housebroke = housebroke
+
+    def is_housebroke(self):
+        if self.housebroke == True:
+            print(f'{self.name} is housebroken!')
+        else:
+            print(f'{self.name} is not housebroken... :(')
+
+
+class Beagle(Dog):
+    def barks_alot(self, barker):
+        pass
+
+
+if __name__ == "__main__":
+    
+    lucky = Dog('Lucky', 2, True)
+    breakpoint()
